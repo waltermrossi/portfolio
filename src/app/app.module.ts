222a9993adca9a6623a9e6e5be67d78f351a66ae
waltermrossi <109000622+waltermrossi@users.noreply.github.com>
2022-07-09 19:03:38 -0300

    Portfolio
    Se sube parte del portfolio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Compo1Component,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
